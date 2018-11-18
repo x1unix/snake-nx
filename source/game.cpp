bf5d56058c05427bc339ac1c44fe770032ca4bd3
x1unix <denis0051@gmail.com>
2018-11-18 03:47:30 +0200

    remove test init

--- source/game.cpp
+++ source/game.cpp
@@ -4,6 +4,8 @@
 #include <time.h>
 #include <switch.h>
 
+#define WITH_TIMER true
+
 using namespace std;
 
 Game::Game(const short vHeight, const short vWidth, PrintConsole* con) : height(vHeight), width(vWidth), console(con) {
@@ -178,95 +180,3 @@ GameState Game::getState() {
 
     return PLAYING;
 }
-
-// void Game::DrawTable() {
-//     consoleClear();
-//     std::string frame;
-
-//     for (int i = 0; i < height; i++) {
-//         for (int j = 0; j < width; j++) {           
-//             if (!i || i == height - 1 || !j || j == width - 1) {
-//                 frame.append("#");
-//             }
-//             else if (i == food_x && j == food_y) {
-//                 frame.append("@");
-//             }
-//             else if (inBoundsOf(i, j)) {
-//                 frame.append("*");
-//             }
-//             else {
-//                 frame.append(" ");
-//             }
-//         }
-//         frame.append("\n");
-//     }
-
-//     printf(frame.c_str());
-//     printf("Your current score is: %d\n", score);
-// }
-
-
-
-// void Game::Process() {
-//     switch (direction) {
-//     case UP:
-//         snake_x--;
-//         break;
-//     case LEFT:
-//         snake_y--;
-//         break;
-//     case DOWN:
-//         snake_x++;
-//         break;
-//     case RIGHT:
-//         snake_y++;
-//         break;
-//     default:
-//         return;
-//     }
-
-//     Move();
-// }
-
-// void Game::Move() {
-//     snake_body[snake_size].index_i = snake_x;
-//     snake_body[snake_size].index_j = snake_y;
-//     if (!snake_x || snake_x == height - 1 || !snake_y || snake_y == width - 1) { // collision logic
-//         status = true;
-//     }
-//     else if (snake_x == food_x && snake_y == food_y) {
-//         snake_size++;
-//         score++;
-//         if (snake_size == max_size) {
-//             win = true;
-//             return;
-//         }
-//         PutFood();
-//     }
-//     else {
-//         for (int index = 0; index < snake_size; index++) {
-//             snake_body[index].index_i = snake_body[index + 1].index_i;
-//             snake_body[index].index_j = snake_body[index + 1].index_j;
-//         }
-//         snake_body[snake_size].index_i = 0;
-//         snake_body[snake_size].index_j = 0;
-//     }
-// #ifdef WITH_TIMER
-//     svcSleepThread(speed);
-// #endif
-// }
-
-// void Game::PutFood() {
-//     srand(time(NULL));
-//     food_x = rand() % (height - 2) + 2;
-//     food_y = rand() % (width - 2) + 2;
-// }
-
-// bool Game::inBoundsOf(int i, int j) {
-//     for (int k = 0; k < snake_size; k++) {
-//         if (i == snake_body[k].index_i && j == snake_body[k].index_j) {
-//             return true;
-//         }
-//     }
-//     return false;
-// }
--- source/main.cpp
+++ source/main.cpp
@@ -15,7 +15,7 @@
 // Speed in nanoseconds
 // 300 ms  -  300000000
 //   5 sec -  5000000000
-#define SPEED 300000000ULL
+#define SPEED 150000000ULL
 #define LEVEL 1
 
 // Messages
@@ -53,61 +53,16 @@ void onGameFinish(GameState state, int score) {
     printf(MSG_SCORE, score);
 }
 
-#ifdef TEST
-int main(int argc, char *argv[]) {
-    PrintConsole* con = consoleInit(NULL);
-    int y = con->windowHeight;
-    int x = con->windowWidth;   // 44?
-
-    for (int i = 0; i < y; i++) {
-        if (i == 0 || i == (y - 1)) {
-            for (int j = 0; j < x; j++) {
-                printf("X");
-            }
-            continue;
-        }
-        printf("Y\n");
-    }
-    //printf("Height: %d\nWidth: %d\n", con->windowHeight, con->windowWidth);
-
-    while (appletMainLoop())
-    {
-        // Scan all the inputs. This should be done once for each frame
-        hidScanInput();
-
-        // hidKeysDown returns information about which buttons have been
-        // just pressed in this frame compared to the previous one
-        u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
-
-        if (kDown & KEY_PLUS)
-            break; // break in order to return to hbmenu
-        
-        consoleUpdate(NULL);
-    }
-
-    consoleExit(NULL);
-    return 0;
-}
-#endif
-#ifndef TEST
 // Main program entrypoint
 int main(int argc, char* argv[])
 {
     bool gameOver = false;
-    // This example uses a text console, as a simple way to output text to the screen.
-    // If you want to write a software-rendered graphics application,
-    //   take a look at the graphics/simplegfx example, which uses the libnx gfx API instead.
-    // If on the other hand you want to write an OpenGL based application,
-    //   take a look at the graphics/opengl set of examples, which uses EGL instead.
     PrintConsole* con = consoleInit(NULL);
 
     // 1px offset workaround to prevent screen autoscroll
     Game game(con->windowHeight - 2, con->windowWidth - 1, con);
     game.setSpeed(SPEED);
     game.Draw();
-
-    // Other initialization goes here. As a demonstration, we print hello world.
-    //printf("Hello World!\n");
 
     // Main loop
     while (appletMainLoop())
@@ -150,4 +105,3 @@ int main(int argc, char* argv[])
     consoleExit(NULL);
     return 0;
 }
-#endif
