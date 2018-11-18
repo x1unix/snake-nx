b3507f6d21b8e9dd424bcb4ba7123b7020b2e326
x1unix <denis0051@gmail.com>
2018-11-18 02:57:45 +0200

    fullscreen

--- source/main.cpp
+++ source/main.cpp
@@ -7,6 +7,7 @@
 #include <switch.h>
 #include <game.h>
 
+
 ////////////////////////
 // Game env settings
 ///////////////////////
@@ -16,8 +17,6 @@
 //   5 sec -  5000000000
 #define SPEED 300000000ULL
 #define LEVEL 1
-#define V_HEIGHT 25
-#define V_WIDTH 50
 
 // Messages
 #define MSG_WIN "Congratulations! You won the game!"
@@ -44,6 +43,7 @@ Direction decodeButtonDirection(u64 kDown) {
 }
 
 void onGameFinish(GameState state, int score) {
+    consoleClear();
     if (state == WON) {
         cout << MSG_WIN;
     } else {
@@ -53,6 +53,43 @@ void onGameFinish(GameState state, int score) {
     printf(MSG_SCORE, score);
 }
 
+#ifdef TEST
+int main(int argc, char *argv[]) {
+    PrintConsole* con = consoleInit(NULL);
+    int y = con->windowHeight;
+    int x = con->windowWidth;   // 44?
+
+    for (int i = 0; i < y; i++) {
+        if (i == 0 || i == (y - 1)) {
+            for (int j = 0; j < x; j++) {
+                printf("X");
+            }
+            continue;
+        }
+        printf("Y\n");
+    }
+    //printf("Height: %d\nWidth: %d\n", con->windowHeight, con->windowWidth);
+
+    while (appletMainLoop())
+    {
+        // Scan all the inputs. This should be done once for each frame
+        hidScanInput();
+
+        // hidKeysDown returns information about which buttons have been
+        // just pressed in this frame compared to the previous one
+        u64 kDown = hidKeysDown(CONTROLLER_P1_AUTO);
+
+        if (kDown & KEY_PLUS)
+            break; // break in order to return to hbmenu
+        
+        consoleUpdate(NULL);
+    }
+
+    consoleExit(NULL);
+    return 0;
+}
+#endif
+#ifndef TEST
 // Main program entrypoint
 int main(int argc, char* argv[])
 {
@@ -62,9 +99,10 @@ int main(int argc, char* argv[])
     //   take a look at the graphics/simplegfx example, which uses the libnx gfx API instead.
     // If on the other hand you want to write an OpenGL based application,
     //   take a look at the graphics/opengl set of examples, which uses EGL instead.
-    consoleInit(NULL);
+    PrintConsole* con = consoleInit(NULL);
 
-    Game game(V_HEIGHT, V_WIDTH);
+    // 1px offset workaround to prevent screen autoscroll
+    Game game(con->windowHeight - 2, con->windowWidth - 1);
     game.setSpeed(SPEED);
 
     // Other initialization goes here. As a demonstration, we print hello world.
@@ -111,3 +149,4 @@ int main(int argc, char* argv[])
     consoleExit(NULL);
     return 0;
 }
+#endif
